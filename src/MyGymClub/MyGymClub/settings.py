86971e2dd7d16cdcce8064b17f22ae81e92fa6ff
hamzajg <hamzajg@gmail.com>
2018-01-14 18:52:51 +0900

    Init DB Connection..

--- src/MyGymClub/MyGymClub/settings.py
+++ src/MyGymClub/MyGymClub/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'MyGymClubApp',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +76,18 @@ WSGI_APPLICATION = 'MyGymClub.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mygymclub_db',
+        'USER': 'root',
+        'PASSWORD': 'lamppass',
+        'HOST': '/opt/lampstack-7.1.4-0/mysql/tmp/mysql.sock',  
+        'PORT': '3306',
     }
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
--- src/MyGymClub/MyGymClubApp/models.py
+++ src/MyGymClub/MyGymClubApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    memberId = models.AutoField(primary_key=True)
+    memberCode = models.CharField(max_length=20)
+    memberFullName = models.CharField(max_length=100)
+    memberAddress = models.CharField(max_length=100)
+    memberPhoneNum = models.IntegerField()
+
+    def __init__(self, memberCode):
+        self.memberCode = memberCode
+
+    def __str(self):
+        return '%d: %s' % (self.memberCode, self.memberCode)
+
+    class Meta:
+        db_table = "member"
+        unique_together = ('memberId', 'memberCode')
+        ordering = ['memberCode']
