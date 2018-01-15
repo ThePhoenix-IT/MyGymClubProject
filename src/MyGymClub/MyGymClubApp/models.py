af4c161c74df26f56bf317d618cbbd2f97d05f2e
Hamza <hamzajg@gmail.com>
2018-01-15 16:52:30 +0100

    Update models.py

--- src/MyGymClub/MyGymClubApp/models.py
+++ src/MyGymClub/MyGymClubApp/models.py
@@ -8,6 +8,7 @@ class Member(models.Model):
     memberFullName = models.CharField(max_length=100)
     memberAddress = models.CharField(max_length=100)
     memberPhoneNum = models.IntegerField()
+    creationDate = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __init__(self, memberCode):
         self.memberCode = memberCode
@@ -19,3 +20,15 @@ class Member(models.Model):
         db_table = "member"
         unique_together = ('memberId', 'memberCode')
         ordering = ['memberCode']
+        
+        
+ class EquipmentStatus(models.Model):
+    equipemntStatusId = models.AutoField(primary_key=True)
+    equipmentStatusDesc = models.CharField(max_length=50)
+    creationDate = models.DateTimeField(auto_now_add=True, blank=True)
+    
+ class Equipment(models.Model):
+    equipemntId = models.AutoField(primary_key=True)
+    equipmentCode = models.CharField(max_length=20)
+    equipmentStatusId = models.ForeignKey(EquipmentStatus, on_delete=models.CASCADE)
+    creationDate = models.DateTimeField(auto_now_add=True, blank=True)
